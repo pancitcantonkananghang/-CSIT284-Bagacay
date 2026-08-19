78fdfad0aa79101770e411992994e966ab218b4b
LAB12_USER ON WS02 <L12X19W02@LabHCI.net>
2026-08-19 21:00:58 +0800

    wa ko ka commit sa uban sir

--- lab_act_2/lib/main.dart
+++ lab_act_2/lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World')));
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
 }
